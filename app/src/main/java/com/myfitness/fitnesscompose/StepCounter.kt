ae2b37444d4852a4f208853dc008ac0b07f18bdf
Khuzin Rinat <khuzin.rinat.work@gmail.com>
2021-12-18 11:59:40 +0500

    add: activity step counter;

---
+++ app/src/main/java/com/myfitness/fitnesscompose/StepCounter.kt
@@ -0,0 +1,16 @@
+package com.myfitness.fitnesscompose
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class StepCounter : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_step_counter)
+    }
+
+
+    
+}
